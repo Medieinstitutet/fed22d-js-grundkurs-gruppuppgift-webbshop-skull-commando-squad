5ebf762e14e0df6e83e07be07216a3f23731a61c
robin-sevelin <robinsevelin@hotmail.com>
2022-11-09 09:59:37 +0100

    working on hamburger

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,20 @@
-const hejHej = "Hallå!"
+const hejHej = "Hallå!";
 
 console.log(hejHej);
+
+// testar js med hamburger menu, work in progress..
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".munk-nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  navMenu.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
