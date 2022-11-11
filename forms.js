6c75ac57b151b780c3504d287978efe857d250a9
Sebastian Post <sebastianpost@protonmail.com>
2022-11-11 10:24:24 +0100

    Forms JS

--- forms.js
+++ forms.js
@@ -1,1 +1,23 @@
-console.log('Hej')
+let cardChecked = false;
+let btn;
+
+
+
+function isCardChecked(){
+    if (cardRadio.checked = true) {
+        console.log('Hej');   
+    }
+    else{
+        console.log('Hej då')
+    }
+}
+
+isCardChecked();
+
+let sum = 50;
+
+if (sum = 50) {
+    console.log('ldfsajgnbljkdösg');
+} else {
+    
+}
