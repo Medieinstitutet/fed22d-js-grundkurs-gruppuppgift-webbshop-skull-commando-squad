0ccc651da902f90a9356cdad7119fecf51a499bd
robin-sevelin <robinsevelin@hotmail.com>
2022-11-18 19:04:27 +0100

    donut layout js

--- donuts.js
+++ donuts.js
@@ -1,75 +1,98 @@
 const donuts = [
   {
-    donutName: "choclate",
+    name: "choclate",
     price: 15,
     rating: 3,
     img: "assets/img/Chocolate-1.png",
   },
   {
-    donutName: "vanilla",
+    name: "vanilla",
     price: 20,
     rating: 1,
     img: "assets/img/vanilla-ice.png",
   },
   {
-    donutName: "strawberry",
+    name: "strawberry",
     price: 35,
     rating: 5,
     img: "assets/img/strawberry.png",
   },
   {
-    donutName: "fudge",
+    name: "fudge",
     price: 45,
     rating: 4,
     img: "assets/img/salted-caramell.png",
   },
   {
-    donutName: "bacon",
+    name: "bacon",
     price: 55,
     rating: 5,
     img: "assets/img/bacon.PNG",
   },
   {
-    donutName: "ribeye",
+    name: "ribeye",
     price: 35,
     rating: 1,
     img: "assets/img/rib-eye.PNG",
   },
   {
-    donutName: "chicken",
+    name: "chicken",
     price: 35,
     rating: 3,
     img: "assets/img/chicken.PNG",
   },
   {
-    donutName: "pulledBeef",
+    name: "pulledBeef",
     price: 45,
     rating: 4,
     img: "assets/img/pulled-beef.PNG",
   },
   {
-    donutName: "krasse",
+    name: "krasse",
     price: 60,
     rating: 1,
     img: "assets/img/water-cress.png",
   },
   {
-    donutName: "maskros",
+    name: "maskros",
     price: 45,
     rating: 2,
     img: "assets/img/dandelion.png",
   },
   {
-    donutName: "majs",
+    name: "majs",
     price: 60,
     price: 60,
     rating: 5,
     img: "assets/img/corn-hole.PNG",
   },
   {
-    donutName: "sjögräs",
+    name: "sjögräs",
     price: 25,
     rating: 1,
     img: "assets/img/sea-weed.png",
   },
 ];
+
+console.log(donuts);
+
+const donutContainer = document.querySelector(".donutContainer");
+
+function renderDonuts() {
+  donutContainer.innerHTML = "";
+
+  for (let i = 0; i < donuts.length; i++) {
+    donutContainer.innerHTML += `
+        <article class="donut">
+          <h2>${donuts[i].name}</h2>
+          <img src="${donuts[i].img}" />
+          <span class="price">${donuts[i].price} kr</span>
+          Antal köpta: <span class="amount">${donuts[i].amount}</span>
+          <button class="add" data-id="${i}">-</button>
+          <button class="add" data-id="${i}">+</button>
+        </article>
+    `;
+  }
+}
+
+renderDonuts();
