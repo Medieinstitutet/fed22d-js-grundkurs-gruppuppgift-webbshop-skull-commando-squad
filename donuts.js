18f691d474349e6a4b38493787b642de649abc77
robin-sevelin <robinsevelin@hotmail.com>
2022-11-18 14:04:28 +0100

    index layout

---
+++ donuts.js
@@ -0,0 +1,14 @@
+const donuts = [
+  { donutName: "choclate", price: 15, rating: 3 },
+  { donutName: "vanilla", price: 20, rating: 1 },
+  { donutName: "strawberry", price: 35, rating: 5 },
+  { donutName: "fudge", price: 45, rating: 4 },
+  { donutName: "bacon", price: 55, rating: 5 },
+  { donutName: "ribeye", price: 35, rating: 1 },
+  { donutName: "chicken", price: 35, rating: 3 },
+  { donutName: "pulledBeef", price: 45, rating: 4 },
+  { donutName: "krasse", price: 60, rating: 1 },
+  { donutName: "maskros", price: 45, rating: 2 },
+  { donutName: "majs", price: 60, price: 60, rating: 5 },
+  { donutName: "sjögräs", price: 25, rating: 1 },
+];
