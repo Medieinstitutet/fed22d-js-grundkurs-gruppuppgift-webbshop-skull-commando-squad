af18dd3129bd72585b4671930d8ad81c54342ee6
Sebastian Post <sebastianpost@protonmail.com>
2022-11-21 15:18:02 +0100

    Börjar likna något nu iaf.

--- karusell/karusell.js
+++ karusell/karusell.js
@@ -7,10 +7,28 @@ const image1 = [
 ];
 
 const image = document.querySelector('#image')
+const leftButton = document.querySelector('#leftButton')
+const rightButton = document.querySelector('#rightButton')
+
+rightButton.addEventListener("click", rightClick);
+leftButton.addEventListener("click", leftClick);
+
+let currentImageIndex = 0;
 
 function init() {
-    image.setAttribute('src', image1[0].url);
-    console.log('Funkar')
+image.setAttribute('src', image1[0].url);
 };
+
+function leftClick() {
+    console.log(currentImageIndex);
+
+ currentImageIndex--
+}
+
+
+function rightClick() {
+ console.log(currentImageIndex);
+ currentImageIndex++
+}
 
 init();
