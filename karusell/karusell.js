629ad82a2ff9470b5486478faa48282f048889a2
Sebastian Post <sebastianpost@protonmail.com>
2022-11-23 10:30:31 +0100

    Lite småfix

--- karusell/karusell.js
+++ karusell/karusell.js
@@ -10,25 +10,30 @@ const imageOne = document.querySelector('#imageOne')
 const imageTwo = document.querySelector('#imageTwo')
 const leftButton = document.querySelector('#leftButton')
 const rightButton = document.querySelector('#rightButton')
-
-rightButton.addEventListener("click", rightClick);
-leftButton.addEventListener("click", leftClick);
-
 let currentImageIndex = 0;
+let firstImageOnTop = true;
+
+
 
 function init() {
 imageOne.setAttribute('src', images[0].url);
 };
 
+rightButton.addEventListener("click", rightClick);
+leftButton.addEventListener("click", leftClick);
+
+
 function leftClick() {
     console.log(currentImageIndex);
     currentImageIndex = Number(currentImageIndex -= 1);
+    init();
 }
 
 
 function rightClick() {
     console.log(currentImageIndex);
     currentImageIndex = Number(currentImageIndex += 1);
+    init(); 
 }
 
 init(); 
