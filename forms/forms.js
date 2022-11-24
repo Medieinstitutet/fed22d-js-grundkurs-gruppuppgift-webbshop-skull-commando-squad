9ca5b3a246bd4a0f163df1be014e9fb3e0385d72
Veronica <veronica.bergstrom@medieinstitutet.se>
2022-11-24 15:09:27 +0100

    merge form to main files

--- forms/forms.js
+++ forms/forms.js
@@ -1,25 +1,40 @@
-const cardRadio = document.querySelector("#cardRadio")
-cardRadio.addEventListener('click', cardOr);
-const invoiceRadio = document.querySelector("#invoiceRadio")
-invoiceRadio.addEventListener('click', cardOr);
+const cardRadio = document.querySelector("#cardRadio");
+cardRadio.addEventListener("click", cardOr);
+const invoiceRadio = document.querySelector("#invoiceRadio");
+invoiceRadio.addEventListener("click", cardOr);
+const openFormBtn = document.querySelector(".submit-order");
+openFormBtn.addEventListener("click", showForm);
+const closeFormBtn = document.querySelector("#close-form");
+closeFormBtn.addEventListener("click", hideForm);
 
-function cardOr() {
-    if (cardRadio.checked) {
-        console.log('kort'); 
-        document.getElementById("cardDetails").style.display = "flex";
-        document.getElementById("cardDetails").style.flexDirection = "column";
-    }if(invoiceRadio.checked){
-        console.log('faktura'); 
-        document.getElementById("cardDetails").style.display = "none";
-    }
+function showForm() {
+  document.getElementById("order-form").classList.remove("hidden");
 }
 
-const usageChecked = document.querySelector('#usage');
-const submitBtn = document.querySelector('submit');
-usageChecked.addEventListener('click', checkChecked);
+function hideForm() {
+  document.getElementById("order-form").classList.add("hidden");
+}
 
-function checkChecked(){
-    if (true) {
-        console.log('hej')
-        document.getElementById("submitBtn").disabled = false;
-    }}
+function cardOr() {
+  if (cardRadio.checked) {
+    console.log("kort");
+    document.getElementById("cardDetails").style.display = "flex";
+    document.getElementById("cardDetails").style.flexDirection = "column";
+  }
+  if (invoiceRadio.checked) {
+    console.log("faktura");
+    document.getElementById("cardDetails").style.display = "none";
+  }
+}
+
+const usageChecked = document.querySelector("#usage");
+const submitBtn = document.querySelector("submit");
+usageChecked.addEventListener("click", checkChecked);
+
+const payBtn = document.querySelector(".pay-button");
+console.log(payBtn, "klickad");
+
+function checkChecked() {
+  if (checkChecked) {
+  }
+}
